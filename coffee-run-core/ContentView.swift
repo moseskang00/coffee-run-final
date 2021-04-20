342cb31b6cdec50e165d818ceedcf70811785317
Moses Kang <mkang41@asu.edu>
2021-04-20 09:47:21 -0700

    Initial Commit

---
+++ coffee-run-core/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffee-run-core
+//
+//  Created by Moses Kang on 4/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coffee-run-core/coffee_run_coreApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_run_coreApp.swift
+//  coffee-run-core
+//
+//  Created by Moses Kang on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct coffee_run_coreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
